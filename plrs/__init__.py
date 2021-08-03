51e3bb10840db5954c62f0c4bbf4a7e0dc8c99a8
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-08-03 12:06:59 -0700

    Add Settings to init

--- plrs/__init__.py
+++ plrs/__init__.py
@@ -2,6 +2,12 @@ from .plrs import *
 
 EOF_TOKEN = Token("", Lexer.EOF)
 
+
+class Settings:
+    PARSE_STRING = 1
+    ALL = PARSE_STRING
+
+
 __version__ = "0.1.1"
 __author__ = "jakeroggenbuck"
 __license__ = "MIT"
